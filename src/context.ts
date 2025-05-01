e099b8cd07bbfbfaf24e987966a7701e4d8fd4ad
delta <delta@deltalab.dev>
2025-05-01 21:35:51 +0900

    fix: use correct input key for builder name

--- src/context.ts
+++ src/context.ts
@@ -31,7 +31,7 @@ export interface Inputs {
 export async function getInputs(): Promise<Inputs> {
   return {
     version: core.getInput('version'),
-    name: core.getInput('name') || await getBuilderName(core.getInput('driver') || 'docker-container'),
+    name: core.getInput('builder-name') || await getBuilderName(core.getInput('driver') || 'docker-container'),
     driver: core.getInput('driver') || 'docker-container',
     driverOpts: Util.getInputList('driver-opts', {ignoreComma: true, quote: false}),
     buildkitdFlags: core.getInput('buildkitd-flags'),
